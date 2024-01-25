877b38177ff89524d997d381ab704409ee7fc3c1
dewacahaya <dewacahaya30@gmail.com>
2024-01-25 14:35:52 +0800

    configure tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins"]
+      },
+      colors: {
+        button: ["#F8B319"],
+        bgweb: ["#000000"],
+        searchbar: ["#21242D"],
+        fontwhite: ["#FCFDFD"]
       },
       screens: {
         "2xl": "1328px"
